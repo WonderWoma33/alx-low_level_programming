87046b98301b907af749b7a3f97343857ec8c14b
root <idikajessy@gmail.com>
2022-07-01 09:10:50 -0700

    Ram

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
-
 /**
+ * main - Prints the size of various types on the computer
  *
- *  main - Entry point
- *
- *  Return: Always 0 (success)
- *
+ * Return: Exit program 
  */
 
 int main(void)
-
 {
-
-	        int i;
-
-                long int l;
-
-	        long long int ll;
-
-	        float d;
-
-                char c;
-
-
-
-        printf("Size of a char: %lu byte(s)\n", sizeof(c));
-
-        printf("Size of an int: %lu byte(s)\n", sizeof(i));
-
-        printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-
-	printf("Size of a float: %lu byte(s)\n", sizeof(d));
- 
-	return (0);
-
+/* sizeof evaluates the size of a variable */  
+ printf("Size of a char: %d byte(s)\n", sizeof(char));
+ printf("Size of an int: %d byte(s)\n", sizeof(int));
+ printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+ printf("Size of a float: %d byte(s)\n", sizeof(float));
+ return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
