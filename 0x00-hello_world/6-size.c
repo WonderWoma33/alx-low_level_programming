21361ce1c2b21f434e8c07b924aacb3e7a69a517
root <idikajessy@gmail.com>
2022-07-01 03:36:26 -0700

    Aboad

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
 /**
- *  main - Enty point
- *   
- *  Return: Always 0 (Success)
+ *
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ *
  */
 
 int main(void)
+
 {
-	char e;
-	int a;
-	long int b;
-	long long int c;
-	float d;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	        int i;
 
-return (0);
+                long int l;
+
+	        long long int ll;
+
+	        float d;
+
+                char c;
+
+
+
+        printf("Size of a char: %lu byte(s)\n", sizeof(c));
+
+        printf("Size of an int: %lu byte(s)\n", sizeof(i));
+
+        printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
+ 
+	return (0);
 
 }
+
+
+
+
