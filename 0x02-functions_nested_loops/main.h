57302fd010854d7deea7f1a07e521477d24184a1
root <idikajessy@gmail.com>
2022-07-05 14:14:25 -0700

    Lost

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-char text[9] ="_putchar";
-     int i= 0;
+char text[9] = "_putchar";
+     int i = 0;
 
-     for (i= 0; i < 8;i++)
+     for (i = 0; i < 8;i++)
      {
          _putchar(text[i]);
      }   
