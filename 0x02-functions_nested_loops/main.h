f03c421cab9f5b2a450d9b971efc8c3f55d12ff9
root <idikajessy@gmail.com>
2022-07-05 14:16:17 -0700

    Good

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**  main - prints putchar
+ * main - Entry begins
+ * Return: nothing, return void
+ */
 int main(void)
 {
-char text[9] = "_putchar";
-     int i = 0;
-
-     for (i = 0; i < 8;i++)
-     {
-         _putchar(text[i]);
-     }   
-    _putchar('\n');
+_putchar(' ');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
-}	
+}
