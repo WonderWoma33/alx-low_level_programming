84f6b18cb8f45702f979f483a05f477a4a353245
root <idikajessy@gmail.com>
2022-07-07 03:54:34 -0700

    Pen

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
+#include "holberton.h"
 
-void positive_or_negative(int i);
+/**
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
 
-#endif /* MAIN_H */
+int main(void)
+{
+int month;
+int day;
+int year;
+
+month = 4;
+day = 01;
+year = 1997;
+
+printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+day = convert_day(month, day);
+
+print_remaining_days(month, day, year);
+
+return (0);
+}
