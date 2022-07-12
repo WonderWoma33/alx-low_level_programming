45ad019c3abca2a8029395df8ca5466b1381eb66
root <idikajessy@gmail.com>
2022-07-12 05:47:32 -0700

    task 1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 
 /**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: int length of the string
+ */
 
 int _strlen(char *s)
 {
-int c = 0;
-for (; *s != '\0'; s++)
-{
-c++;
-}
-return (c);
+int length = 0;
+while (s[length])
+length++;
+return (length);
 }
