e5fa979892ddb123125020c5be6fde0cf1b12c5f
root <idikajessy@gmail.com>
2022-07-12 05:40:34 -0700

    short file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,12 @@
-**
+#include "holberton.h"
 
+/**
   * _strlen - Returns the length of a string
   * @s: String to count
   *
   * Return: String length
   */
+
 int _strlen(char *s)
 {
 int c = 0;
