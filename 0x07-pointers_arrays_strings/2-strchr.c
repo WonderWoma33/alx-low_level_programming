2c2f4a9f4d688ee2722429504df005764e44cb8a
root <idikajessy@gmail.com>
2022-07-18 06:22:07 -0700

    smash

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strchr -> string character
- * @s: string given
- * @c: another char
- * Return: a string
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ * Return: If c is found - a pointer to the first occurence.
+ * If c is not found - NULL.
  */
 
 char *_strchr(char *s, char c)
 {
-int a;
-while (1)
+int index;
+for (index = 0; s[index] >= '\0'; index++)
 {
-a = *s++;
-if (a == c)
-{
-return (s - 1);
+if (s[index] == c)
+return (s + index);
 }
-if (a == 0)
-{
-return (NULL);
-}
-}
+return ('\0');
 }
